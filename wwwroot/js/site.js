1db5ea6dcb3ca01d97501faaaa38a813d69059ea
S t <sthlm_32@hotmail.com>
2022-03-02 23:06:09 +0100

    Cosmetics

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -12,21 +12,24 @@ $(document).ready(function () {
             $("#food-header-" + id).css("display", "inline-block");
             $(".foods-wrapper-row-" + id).slideDown("slow");
             $(".food-list-" + id).css("display", "inline");
-            $(this).parent().parent().css("border-bottom", "none");
             $(this).text("Hide details");
 
             // All .beer-row(s)
-            $(".beer-row .beer-name").css("opacity", "40%");
-            $(".beer-row .beer-apv").css("opacity", "40%");
+            $(".beer-row .beer-name, .beer-row .beer-apv").css("opacity", "40%");
+
+            // The the previous .beer
+            $(this).parent().parent().parent().prev().find(".beer-row").css("border-bottom", "none");
+
+            // Current .beer
+            $(this).parent().parent().parent().css("background", "lightgray");
+            $(this).parent().parent().parent().css("-webkit-box-shadow", "1px 1px 3px 1px #000000");
+            $(this).parent().parent().parent().css("box-shadow", "1px 1px 3px 1px #000000");
+            $(this).parent().parent().parent().css("margin-top", "2em");
 
             // Current .beer-row
             $(this).parent().parent().css("font-size", "125%");
-            $(this).parent().parent().css("border-top", "1px solid #000");
             $(this).parent().parent().css("background", "#b3d8f9");
             $(this).parent().parent().children().css("opacity", "100%");
-
-            // Next .beer-row
-            $(this).parent().parent().nextUntil(".beer-row").last().next().css("border-top", "2px solid rgb(0, 0, 0)");
         } else {
             reset();
         }
@@ -39,11 +42,14 @@ $(document).ready(function () {
             $(".food-list").css("display", "none");
             $(".foods-wrapper-row").slideUp("fast");
             $(".food-list").css("display", "none");
-            $(".beer-row").css("border-bottom", "1px solid #a8a5a5");
             $(".beer-row a").text("Show details");
             $(".beer-row").css("font-size", "100%");
-            $(".beer-row").css("border-top", "none");
-            $(".beer-row").css("background", "none");
+            $(".beer-row").css("border-bottom", "1px solid #a8a5a5");
+            $(".beer-row .beer-name, .beer-row .beer-apv").css("opacity", "100%");
+            $(".beer-row, .beer").css("background", "none");
+            $(".beer").css("-webkit-box-shadow", "none");
+            $(".beer").css("box-shadow", "none");
+            $(".beer").css("margin-top", "0");
         }
     });
 })
